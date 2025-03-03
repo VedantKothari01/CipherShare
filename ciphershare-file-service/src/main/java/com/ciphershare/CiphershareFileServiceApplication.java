7cfc902163a144c10dd8e9f7e4f233eb795f21b7
Vedant Kothari <115355187+VedantKothari01@users.noreply.github.com>
2025-03-03 18:48:32 +0530

    Created MS

---
+++ ciphershare-file-service/src/main/java/com/ciphershare/CiphershareFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ciphershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphershareFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphershareFileServiceApplication.class, args);
+	}
+
+}
---
+++ ciphershare-user-service/src/main/java/com/ciphershare/CiphershareUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ciphershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphershareUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphershareUserServiceApplication.class, args);
+	}
+
+}
