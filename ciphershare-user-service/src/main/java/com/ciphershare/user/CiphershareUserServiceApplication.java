e15235ecc35f6d913a5d6b05a374e36ba3943912
Vedant Kothari <115355187+VedantKothari01@users.noreply.github.com>
2025-03-03 19:37:41 +0530

    User Service Updated

--- ciphershare-user-service/src/main/java/com/ciphershare/user/CiphershareUserServiceApplication.java
+++ ciphershare-user-service/src/main/java/com/ciphershare/user/CiphershareUserServiceApplication.java
@@ -1,9 +1,11 @@
-package com.ciphershare;
+package com.ciphershare.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CiphershareUserServiceApplication {
 
 	public static void main(String[] args) {
