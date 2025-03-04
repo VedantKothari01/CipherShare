fbd1dca553d02921a39ed2a6f95f3913b8a1b0aa
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2025-03-04 13:04:44 +0530

    User Service updated

--- ciphershare-user-service/src/test/java/com/ciphershare/CiphershareUserServiceApplicationTests.java
+++ ciphershare-user-service/src/test/java/com/ciphershare/CiphershareUserServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.ciphershare;
 
+import com.ciphershare.user.CiphershareUserServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CiphershareUserServiceApplication.class)
 class CiphershareUserServiceApplicationTests {
 
 	@Test
