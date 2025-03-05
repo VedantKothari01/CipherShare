d80734f95a1e202dbe4df9ed5212f1fb5272508b
Vedant Kothari <115355187+VedantKothari01@users.noreply.github.com>
2025-03-05 18:45:31 +0530

    Updated Eureka Server

---
+++ ciphershare-discovery-service/src/main/java/com/ciphershare/CiphershareDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ciphershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphershareDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphershareDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ ciphershare-discovery-service/src/test/java/com/ciphershare/CiphershareDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciphershare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiphershareDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
