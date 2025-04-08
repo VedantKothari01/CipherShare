79337561569bd48d26d7f827f7787147510bbd69
Vedant Kothari <115355187+VedantKothari01@users.noreply.github.com>
2025-04-08 23:38:53 +0530

    Added FileUpload and Retrieval - pinata

--- ciphershare-file-service/src/main/java/com/ciphershare/file/service/FileService.java
+++ ciphershare-file-service/src/main/java/com/ciphershare/file/service/FileService.java
@@ -4,21 +4,31 @@ import com.ciphershare.file.entity.File;
 import com.ciphershare.file.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import org.json.JSONObject;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FileService {
+
+    private final WebClient webClient;
+    private final RestTemplate restTemplate;
+
 
     @Autowired
     private FileRepository fileRepository;
@@ -33,13 +43,13 @@ public class FileService {
     private static final String PINATA_DELETE_URL = "https://api.pinata.cloud/pinning/unpin/";
     private static final String PINATA_GATEWAY_URL = "https://gateway.pinata.cloud/ipfs/";
 
-    private final RestTemplate restTemplate;
-
     @Autowired
-    public FileService(FileRepository fileRepository) {
+    public FileService(WebClient.Builder webClientBuilder, FileRepository fileRepository) {
+        this.webClient = webClientBuilder.baseUrl("https://api.pinata.cloud").build();
         this.fileRepository = fileRepository;
         this.restTemplate = createRestTemplate();
     }
+
 
     private RestTemplate createRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
@@ -49,31 +59,26 @@ public class FileService {
         restTemplate.setMessageConverters(messageConverters);
         return restTemplate;
     }
+    public String uploadFile(MultipartFile file) throws IOException {
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("file", new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        }).contentType(MediaType.MULTIPART_FORM_DATA);
 
-    public File uploadFile(MultipartFile file) throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("pinata_api_key", pinataApiKey);
-        headers.set("pinata_secret_api_key", pinataApiSecret);
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        HttpEntity<MultipartFile> requestEntity = new HttpEntity<>(file, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(PINATA_UPLOAD_URL, requestEntity, String.class);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            JSONObject jsonResponse = new JSONObject(response.getBody());
-            String cid = jsonResponse.getString("IpfsHash");
-
-            File savedFile = new File();
-            savedFile.setFileName(file.getOriginalFilename());
-            savedFile.setFileSize(file.getSize());
-            savedFile.setFileType(file.getContentType());
-            savedFile.setEncryptedPath(PINATA_GATEWAY_URL + cid);
-
-            return fileRepository.save(savedFile);
-        } else {
-            throw new RuntimeException("Failed to upload file to Pinata: " + response.getBody());
-        }
+        return webClient.post()
+                .uri("/pinning/pinFileToIPFS")
+                .header("pinata_api_key", pinataApiKey)
+                .header("pinata_secret_api_key", pinataApiSecret)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .bodyValue(builder.build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
+
 
     public String getFileUrl(String fileId) {
         File file = fileRepository.findById(fileId).orElseThrow(() -> new RuntimeException("File not found"));
